215e01111da1e9d2fc20b2785c664146b03df32d
canaybozkus <bozkuscanay@gmail.com>
2020-11-29 20:53:32 +0300

    AppBar

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:repetapp/utilities/constants.dart';
 import 'package:repetapp/widgets/circular_bottom_bar.dart';
+import 'package:repetapp/widgets/double_circle_background.dart';
 
 class MainScreen extends StatelessWidget {
   @override
@@ -9,14 +10,25 @@ class MainScreen extends StatelessWidget {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Center(
-        child: Container(
-          color: Colors.red,
-          width: double.infinity,
-          height: height,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          'Hatırlatıcı',
+          style: TextStyle(
+            color: kPrimaryColor,
+          ),
         ),
+        actions: [
+          Icon(Icons.menu, color: kPrimaryColor,),
+          SizedBox(width: 15.0,),
+        ],
       ),
-      bottomNavigationBar: CircularBottomBar(height: height, width: width),
+      body: DoubleCircleBackground(
+        child: Text('text'),
+      ),
+      bottomNavigationBar: CircularBottomBar(height: height, width: width,),
     );
   }
 }
