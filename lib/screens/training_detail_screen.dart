6eb6765871998e5bf583caf29cc09a3dbd27805a
canaybozkus <bozkuscanay@gmail.com>
2020-12-09 14:27:43 +0300

    TrainingDetailScreen

--- lib/screens/training_detail_screen.dart
+++ lib/screens/training_detail_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:repetapp/utilities/constants.dart';
+import 'package:repetapp/widgets/circular_bottom_bar.dart';
+import 'package:repetapp/widgets/default_elevation.dart';
 
 class TrainingDetailScreen extends StatefulWidget {
   static const routeName = 'TrainingDetailScreen';
@@ -10,6 +12,8 @@ class TrainingDetailScreen extends StatefulWidget {
 class _TrainingDetailScreenState extends State<TrainingDetailScreen> {
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -24,6 +28,115 @@ class _TrainingDetailScreenState extends State<TrainingDetailScreen> {
             width: 15.0,
           ),
         ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(
+            vertical: height * 0.038, horizontal: width * 0.05),
+        child: DefaultElevation(
+          child: Container(
+            height: height * 0.7,
+            width: width * 0.9,
+            color: Colors.grey.shade50,
+            child: Column(
+              children: [
+                DefaultElevation(
+                  child: Container(
+                    width: width * 0.9,
+                    height: height * 0.3,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Image(
+                          image: AssetImage('assets/images/dog-img.jpeg'),
+                          height: height * 0.2,
+                        ),
+                        Text(
+                          'Adım 1',
+                          style: TextStyle(color: Colors.grey.shade700),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  height: height * 0.3,
+                  width: width * 0.9,
+                  child: ListView(
+                    padding: EdgeInsets.symmetric(
+                        vertical: height * 0.03, horizontal: width * 0.03),
+                    children: [
+                      Text(
+                        'Köpeklerin sizi mutlu etmek için yapamayacağı ' +
+                            'şey yoktur - bu yüzden çoğu kolayca eğitilebilir. ' +
+                            'Başlamadan önce, onun bilmesini istediğiniz ' +
+                            'temel komutların bir listesini yapın: “otur”, “dur”, ' +
+                            '“gel”, “aşağı” veya “hayır” .',
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        '(bu daima ileride faydalı olur). ',
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: width * 0.9,
+                  height: height * 0.08,
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: width * 0.01),
+                        height: height * 0.025,
+                        width: height * 0.025,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: kPrimaryColor,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: width * 0.01),
+                        height: height * 0.025,
+                        width: height * 0.025,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.grey.shade500,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: width * 0.01),
+                        height: height * 0.025,
+                        width: height * 0.025,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.grey.shade500,
+                        ),
+                      ),
+                      Container(
+                        width: width * 0.15,
+                        height: height * 0.05,
+                        decoration: BoxDecoration(
+                          color: kPrimaryColor,
+                          borderRadius: BorderRadius.circular(width * 0.05),
+                        ),
+                        child: Icon(
+                          Icons.arrow_forward_ios,
+                          color: Colors.white,
+                          size: height * 0.04,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      bottomNavigationBar: CircularBottomBar(
+        height: height,
+        width: width,
+        pageNumber: 5,
       ),
     );
   }
