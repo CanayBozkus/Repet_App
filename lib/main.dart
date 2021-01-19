26bf96ccfe7fe1929ea9f7ac17227fb2282485ef
canaybozkus <bozkuscanay@gmail.com>
2021-01-19 23:23:06 +0300

    Registration, paw svg

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        primaryColor: kPrimaryColor,
         textTheme: TextTheme(
           bodyText2: TextStyle(
             fontSize: 16.0,
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -16,25 +16,21 @@ class RegistrationScreen extends StatefulWidget {
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
   UserModel _newUser = UserModel();
-
-  Widget _listViewContentGenerator(label, onChanged, {obsecure, keyBoardType}){
-    return  Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: BaseInputField(
-        label: label,
-        onChanged: onChanged,
-        obsecure: obsecure,
-        keyboardType: keyBoardType ?? KeyboardTypes.text,
-      ),
-    );
-  }
-
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
       resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: Text(
+            'Register',
+          style: Theme.of(context).appBarTheme.textTheme.headline6.copyWith(color: Colors.white),
+        ),
+        leading: Container(),
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
       body: SafeArea(
         child: GestureDetector(
           onTap: () {
@@ -42,149 +38,19 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
           },
           child: Container(
             padding: EdgeInsets.symmetric(
-              horizontal: width * 0.1,
+              horizontal: width * 0.05,
             ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  height: height * 0.1,
-                  child: Align(
-                    alignment: Alignment.bottomRight,
-                    child: Text(
-                      'Kayıt Ol',
-                      style: TextStyle(
-                        fontSize: height * 0.05,
-                        fontWeight: FontWeight.w700,
-                        color: kPrimaryColor,
-                      ),
+            child: Form(
+                child: ListView(
+                  children: [
+                    TextFormField(
+
                     ),
-                  ),
+                    TextFormField(),
+                    TextFormField(),
+                    TextFormField(),
+                  ],
                 ),
-                SizedBox(
-                  height: height * .1,
-                ),
-                Container(
-                  height: height*0.5,
-                  child: ListView(
-                    children: [
-                      _listViewContentGenerator('Ad Soyad', (value)=> _newUser.nameSurname = value,),
-                      _listViewContentGenerator('Email', (value)=> _newUser.email = value.toString().trim(), keyBoardType: KeyboardTypes.emailAddress),
-                      _listViewContentGenerator('Şifre', (value)=> _newUser.password = value, obsecure: true),
-                      _listViewContentGenerator('Tel No', (value)=> _newUser.phoneNumber = int.parse(value), keyBoardType: KeyboardTypes.number),
-                      _listViewContentGenerator('Yaş', (value)=> _newUser.age = int.parse(value), keyBoardType: KeyboardTypes.number),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: DefaultElevation(
-                          child: Row(
-                            children: [
-                              Expanded(
-                                  child: Container(
-                                    height: height*0.09,
-                                    padding: EdgeInsets.symmetric(horizontal: 12),
-                                    child: Row(
-                                      children: [
-                                        Icon(FontAwesomeIcons.transgenderAlt, color: Colors.grey,),
-                                        SizedBox(width: 12,),
-                                        Text(
-                                          'Cinsiyet',
-                                          style: TextStyle(
-                                            color: Color(0xffd0d0d0),
-                                            fontWeight: FontWeight.w500,
-                                            fontSize: 18.0,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  )
-                              ),
-                              Container(
-                                height: height*0.09,
-                                width: 1,
-                                color: Colors.grey.shade400,
-                              ),
-                              Expanded(
-                                child: Container(
-                                  height: height*0.09,
-                                  padding: EdgeInsets.symmetric(horizontal: width*0.05),
-                                  alignment: Alignment.center,
-                                  child: DropdownButton<String>(
-                                    isExpanded: true,
-                                    underline: Container(),
-                                    iconEnabledColor: kPrimaryColor,
-                                    value: _newUser.gender,
-                                    icon: Icon(Icons.arrow_downward),
-                                    onChanged: (String newValue){
-                                      setState(() {
-                                        _newUser.gender = newValue;
-                                      });
-                                    },
-                                    items: <String>['Female', 'Male', 'Other'].map<DropdownMenuItem<String>>((String value){
-                                      return DropdownMenuItem<String>(
-                                        child: Text(value),
-                                        value: value,
-                                      );
-                                    }).toList(),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: height * .1,
-                ),
-                Material(
-                  shadowColor: Colors.grey.shade400,
-                  elevation: 10,
-                  child: FlatButton(
-                    color: kPrimaryColor,
-                    padding: EdgeInsets.symmetric(vertical: 10.0),
-                    minWidth: double.infinity,
-                    child: Text(
-                      'Kayıt Ol',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                    onPressed: () async {
-                      bool response = await _newUser.createUser();
-                      if(response){
-                        Navigator.pushNamed(context, LoginScreen.routeName);
-                      }
-                    },
-                  ),
-                ),
-                SizedBox(
-                  height: height * .02,
-                ),
-                Container(
-                  width: double.infinity,
-                  child: RichText(
-                    textAlign: TextAlign.center,
-                    text: TextSpan(
-                      text: 'Hesabınız var mı?  ',
-                      style: kDefaultTextStyle,
-                      children: [
-                        TextSpan(
-                          text: 'Giriş Yap',
-                          style:
-                          kDefaultTextStyle.copyWith(color: Colors.black),
-                          recognizer: TapGestureRecognizer()..onTap = (){
-                            Navigator.pushReplacementNamed(context, LoginScreen.routeName);
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
             ),
           ),
         ),
