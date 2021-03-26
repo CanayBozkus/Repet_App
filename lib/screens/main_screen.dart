3ebeef5ad42a790dc333408a7340a558abd46b66
Utku Uyar <56119416+UtkuUyar@users.noreply.github.com>
2021-03-26 21:13:02 +0300

    UtkuUyar added removing a reminder logic.

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -13,6 +13,7 @@ import 'package:provider/provider.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:repetapp/widgets/time_selector.dart';
 import 'package:repetapp/utilities/extensions.dart';
+
 class MainScreen extends StatefulWidget {
   static const routeName = 'MainScreen';
 
@@ -21,10 +22,16 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  void _openBottomSheet({Remainders remainder, }){
-    final pet = context.read<GeneralProviderData>().pets[context.read<GeneralProviderData>().currentShownPetIndex];
+  void _openBottomSheet({
+    Remainders remainder,
+  }) {
+    final pet = context
+        .read<GeneralProviderData>()
+        .pets[context.read<GeneralProviderData>().currentShownPetIndex];
     final List routine = pet.routines[remainderTitles[remainder].toLowerCase()];
     bool addNew = false;
+    bool isEditing = false;
+    int currentFocusedReminder = -99;
     int hour = 0;
     int min = 0;
     bool isActive = true;
@@ -34,19 +41,19 @@ class _MainScreenState extends State<MainScreen> {
         borderRadius: BorderRadius.circular(10.0),
       ),
       context: context,
-      builder: (context){
+      builder: (context) {
         return StatefulBuilder(
-          builder: (BuildContext context, StateSetter setState){
+          builder: (BuildContext context, StateSetter setState) {
             return Column(
               children: [
                 Container(
                   height: 80,
                   decoration: BoxDecoration(
                     border: Border(
-                        bottom: BorderSide(
-                          color: Colors.grey.shade300,
-                          width: 1,
-                        )
+                      bottom: BorderSide(
+                        color: Colors.grey.shade300,
+                        width: 1,
+                      ),
                     ),
                   ),
                   child: Row(
@@ -54,10 +61,13 @@ class _MainScreenState extends State<MainScreen> {
                       FlatButton(
                         shape: CircleBorder(),
                         child: Container(
-                          child: Icon(Icons.close, size: 36,),
+                          child: Icon(
+                            Icons.close,
+                            size: 36,
+                          ),
                           padding: EdgeInsets.all(4),
                         ),
-                        onPressed: (){
+                        onPressed: () {
                           Navigator.pop(context);
                         },
                         padding: EdgeInsets.zero,
@@ -75,10 +85,14 @@ class _MainScreenState extends State<MainScreen> {
                       FlatButton(
                         shape: CircleBorder(),
                         child: Container(
-                          child: Icon(Icons.add, size: 36, color: kPrimaryColor,),
+                          child: Icon(
+                            Icons.add,
+                            size: 36,
+                            color: kPrimaryColor,
+                          ),
                           padding: EdgeInsets.all(4),
                         ),
-                        onPressed: (){
+                        onPressed: () {
                           controller.jumpTo(0);
                           setState(() {
                             addNew = true;
@@ -93,102 +107,161 @@ class _MainScreenState extends State<MainScreen> {
                   child: ListView(
                     controller: controller,
                     children: [
-                      addNew ? Container(
-                        padding: EdgeInsets.symmetric(horizontal: 16),
-                        child: Stack(
-                          children: [
-                            Column(
-                              children: [
-                                SizedBox(height: 5,),
-                                Container(
-                                  alignment: Alignment.center,
-                                  width: double.maxFinite,
-                                  height: 90,
-                                  child: TimeSelector(hourOnChanged: (value) => hour = value, minuteOnChanged: (value) => min = value, center: true,),
-                                ),
-                                SizedBox(height: 15,),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    BaseButton(
-                                      width: 80,
-                                      fontSize: 13,
-                                      text: 'Delete',
-                                      onPressed: (){},
-                                      backgroundColor: kColorRed,
-                                    ),
-                                    SizedBox(width: 10,),
-                                    BaseButton(
-                                      width: 80,
-                                      fontSize: 13,
-                                      text: 'Done',
-                                      onPressed:() async {
-                                        DateTime now = DateTime.now();
-                                        DateTime time = DateTime(now.year, now.month, now.day , hour, min, 0);
-                                        await Provider.of<GeneralProviderData>(context, listen: false).addNewRemainder(pet, remainder, time, isActive);
+                      addNew || isEditing
+                          ? Container(
+                              padding: EdgeInsets.symmetric(horizontal: 16),
+                              child: Stack(
+                                children: [
+                                  Column(
+                                    children: [
+                                      SizedBox(
+                                        height: 5,
+                                      ),
+                                      Container(
+                                        alignment: Alignment.center,
+                                        width: double.maxFinite,
+                                        height: 90,
+                                        child: TimeSelector(
+                                          hourOnChanged: (value) =>
+                                              hour = value,
+                                          minuteOnChanged: (value) =>
+                                              min = value,
+                                          center: true,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 15,
+                                      ),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          BaseButton(
+                                            width: 80,
+                                            fontSize: 13,
+                                            text: 'Delete',
+                                            onPressed: () async {
+                                              if (isEditing) {
+                                                // TODO: Add logic for removing a reminder.
+                                                Provider.of<GeneralProviderData>(
+                                                        context,
+                                                        listen: false)
+                                                    .removeReminder(
+                                                  pet,
+                                                  remainder,
+                                                  currentFocusedReminder,
+                                                );
+                                              }
+                                              setState(() {
+                                                isEditing = false;
+                                                addNew = false;
+                                              });
+                                            },
+                                            backgroundColor: kColorRed,
+                                          ),
+                                          SizedBox(
+                                            width: 10,
+                                          ),
+                                          BaseButton(
+                                            width: 80,
+                                            fontSize: 13,
+                                            text: 'Done',
+                                            onPressed: () async {
+                                              DateTime now = DateTime.now();
+                                              DateTime time = DateTime(
+                                                  now.year,
+                                                  now.month,
+                                                  now.day,
+                                                  hour,
+                                                  min,
+                                                  0);
+                                              // should be updateRemainder if isEditing true
+                                              await Provider.of<
+                                                          GeneralProviderData>(
+                                                      context,
+                                                      listen: false)
+                                                  .addNewRemainder(
+                                                      pet,
+                                                      remainder,
+                                                      time,
+                                                      isActive);
+                                              setState(() {
+                                                addNew = false;
+                                              });
+                                            },
+                                            backgroundColor: kColorGreen,
+                                          ),
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                  Positioned(
+                                    right: 0,
+                                    child: Switch(
+                                      value: isActive,
+                                      onChanged: (value) {
                                         setState(() {
-                                          addNew = false;
+                                          isActive = value;
                                         });
                                       },
-                                      backgroundColor: kColorGreen,
                                     ),
-                                  ],
-                                )
-                              ],
-                            ),
-                            Positioned(
-                              right: 0,
-                              child: Switch(
-                                value: isActive,
-                                onChanged: (value){
-                                  setState(() {
-                                    isActive = value;
-                                  });
-                                },
+                                  ),
+                                ],
                               ),
-                            ),
-                          ],
-                        ),
-                      ) : SizedBox.shrink(),
-                      ...routine.map((element){
+                            )
+                          : SizedBox.shrink(),
+                      ...routine.map((element) {
                         DateTime elementTime = element.time;
-                        return Container(
-                          height: 70,
-                          padding: EdgeInsets.symmetric(horizontal: 16),
-                          decoration: BoxDecoration(
-                            border: Border(
+                        return InkWell(
+                          onLongPress: () => setState(() {
+                            if (!addNew) {
+                              isEditing = true;
+                              currentFocusedReminder = element.id;
+                            }
+                          }),
+                          child: Container(
+                            height: 70,
+                            padding: EdgeInsets.symmetric(horizontal: 16),
+                            decoration: BoxDecoration(
+                              border: Border(
                                 bottom: BorderSide(
                                   color: Colors.grey.shade300,
                                   width: 1,
-                                )
-                            ),
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                elementTime.getHourAndMinuteString(),
-                                style: TextStyle(
-                                  fontSize: 36,
-                                  fontWeight: FontWeight.w400,
                                 ),
                               ),
-                              Switch(
-                                value: element.isActive,
-                                onChanged: (value) async {
-                                  if(value){
-                                    await Provider.of<GeneralProviderData>(context, listen: false).updateRemainderStatus(pet, element, remainder, value);
-                                  }
-                                  else {
-                                    //routine[id][1] = value;
-                                    await Provider.of<GeneralProviderData>(context, listen: false).updateRemainderStatus(pet, element, remainder, value);
-                                  }
-                                  setState(() {
-
-                                  });
-                                },
-                              ),
-                            ],
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  elementTime.getHourAndMinuteString(),
+                                  style: TextStyle(
+                                    fontSize: 36,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                                Switch(
+                                  value: element.isActive,
+                                  onChanged: (value) async {
+                                    if (value) {
+                                      await Provider.of<GeneralProviderData>(
+                                              context,
+                                              listen: false)
+                                          .updateRemainderStatus(
+                                              pet, element, remainder, value);
+                                    } else {
+                                      //routine[id][1] = value;
+                                      await Provider.of<GeneralProviderData>(
+                                              context,
+                                              listen: false)
+                                          .updateRemainderStatus(
+                                              pet, element, remainder, value);
+                                    }
+                                    setState(() {});
+                                  },
+                                ),
+                              ],
+                            ),
                           ),
                         );
                       }).toList(),
@@ -202,20 +275,20 @@ class _MainScreenState extends State<MainScreen> {
       },
     );
   }
+
   Future<bool> getMainScreenData() async {
-    if(context.read<GeneralProviderData>().isMainScreenDataFetched){
+    if (context.read<GeneralProviderData>().isMainScreenDataFetched) {
       return true;
     }
-    try{
+    try {
       await context.read<GeneralProviderData>().getMainScreenData();
       return true;
-    }
-
-    catch(e){
+    } catch (e) {
       print(e);
       return false;
     }
   }
+
   Future<bool> _isLoading;
   @override
   void initState() {
@@ -223,18 +296,21 @@ class _MainScreenState extends State<MainScreen> {
     context.read<GeneralProviderData>().currentUser.setNotificationSettings();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: BaseAppBar(title: 'Hatırlatıcı', context: context,),
+      appBar: BaseAppBar(
+        title: 'Hatırlatıcı',
+        context: context,
+      ),
       body: FutureBuilder(
-        builder: (context, snapshots){
+        builder: (context, snapshots) {
           if (snapshots.connectionState == ConnectionState.none &&
               snapshots.hasData == null) {
             //print('project snapshot data is: ${projectSnap.data}');
             return ErrorScreen();
-          }
-          else if(snapshots.connectionState == ConnectionState.waiting){
+          } else if (snapshots.connectionState == ConnectionState.waiting) {
             return Center(
               child: CircularProgressIndicator(),
             );
@@ -252,13 +328,12 @@ class _MainScreenState extends State<MainScreen> {
                       vertical: 10,
                     ),
                     child: ListView(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: 5, vertical: 0),
+                      padding: EdgeInsets.symmetric(horizontal: 5, vertical: 0),
                       children: [
-                        ...Remainders.values.map((remainder){
+                        ...Remainders.values.map((remainder) {
                           return RemainderRow(
                             remainder: remainder,
-                            onTap: (){
+                            onTap: () {
                               _openBottomSheet(remainder: remainder);
                             },
                           );
