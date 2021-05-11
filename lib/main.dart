910cdb67c7b1bba615caaf7d8f484b8d5b44c852
ituitis-uyaru19 <uyaru19@itu.edu.tr>
2021-05-11 23:01:18 +0300

    set GestureDetector as topmost element to handle unfocus

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'utilities/constants.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -14,44 +15,57 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  bool isLoggedIn(){
-    if(FirebaseAuth.instance.currentUser != null){
+  bool isLoggedIn() {
+    if (FirebaseAuth.instance.currentUser != null) {
       return true;
     }
     return false;
   }
+
   final GeneralProviderData providedData = GeneralProviderData();
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => providedData,),
-      ],
-      child: MaterialApp(
-        theme: ThemeData(
-          primaryColor: kPrimaryColor,
-          textTheme: TextTheme(
-            bodyText2: TextStyle(
-              fontSize: 16.0,
-              fontWeight: FontWeight.w600,
-            ),
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocusedNode = FocusScope.of(context);
+        if (!currentFocusedNode.hasPrimaryFocus &&
+            currentFocusedNode.focusedChild != null) {
+          // currentFocusedNode.focusedChild.unfocus();
+          FocusManager.instance.primaryFocus.unfocus();
+        }
+      },
+      child: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (_) => providedData,
           ),
-          scaffoldBackgroundColor: Colors.white,
-          appBarTheme: AppBarTheme(
-            color: Colors.white,
-            elevation: 0,
-            centerTitle: true,
+        ],
+        child: MaterialApp(
+          theme: ThemeData(
+            primaryColor: kPrimaryColor,
             textTheme: TextTheme(
-              headline6: TextStyle(
-                color: kPrimaryColor,
-                fontSize: 20.0,
-                fontWeight: FontWeight.w500,
+              bodyText2: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            scaffoldBackgroundColor: Colors.white,
+            appBarTheme: AppBarTheme(
+              color: Colors.white,
+              elevation: 0,
+              centerTitle: true,
+              textTheme: TextTheme(
+                headline6: TextStyle(
+                  color: kPrimaryColor,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
             ),
           ),
+          initialRoute: isLoggedIn() ? 'MainScreen' : 'LoginScreen',
+          onGenerateRoute: RouteGenerator.generateRoute,
         ),
-        initialRoute: isLoggedIn() ? 'MainScreen' : 'LoginScreen',
-        onGenerateRoute: RouteGenerator.generateRoute,
       ),
     );
   }
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -19,6 +19,7 @@ class RegistrationScreen extends StatefulWidget {
 class _RegistrationScreenState extends State<RegistrationScreen> {
   UserModel _newUser = UserModel();
   final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
@@ -38,192 +39,190 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
         backgroundColor: Theme.of(context).primaryColor,
       ),
       body: SafeArea(
-        child: GestureDetector(
-          onTap: () {
-            FocusScope.of(context).unfocus();
-          },
-          child: Container(
-            padding: EdgeInsets.symmetric(
-              horizontal: width * 0.05,
-              vertical: height * 0.05,
-            ),
-            alignment: Alignment.center,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Container(
-                  height: height * 0.65,
-                  child: Form(
-                    key: _formKey,
-                    child: ListView(
-                      padding: EdgeInsets.symmetric(horizontal: 2),
-                      children: [
-                        FormGenerator.addInput(
-                          label: 'Mail',
-                          keyboard: KeyboardTypes.emailAddress,
-                          onsaved: (String value) {
-                            _newUser.email = value;
+        child: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: width * 0.05,
+            vertical: height * 0.05,
+          ),
+          alignment: Alignment.center,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Container(
+                height: height * 0.65,
+                child: Form(
+                  key: _formKey,
+                  child: ListView(
+                    padding: EdgeInsets.symmetric(horizontal: 2),
+                    children: [
+                      FormGenerator.addInput(
+                        label: 'Mail',
+                        keyboard: KeyboardTypes.emailAddress,
+                        onsaved: (String value) {
+                          _newUser.email = value;
+                        },
+                        validator: FormGenerator.mailValidator,
+                      ),
+                      FormGenerator.addInput(
+                        label: 'Password',
+                        keyboard: KeyboardTypes.text,
+                        obsecure: true,
+                        onsaved: (String value) {
+                          _newUser.password = value;
+                        },
+                        validator: FormGenerator.passwordValidator,
+                        onchanged: (value) => _newUser.password = value,
+                      ),
+                      FormGenerator.addInput(
+                        label: 'Password',
+                        keyboard: KeyboardTypes.text,
+                        obsecure: true,
+                        validator: (String value) {
+                          if (value != _newUser.password) {
+                            return "Passwords does not match!";
+                          }
+                          return null;
+                        },
+                      ),
+                      FormGenerator.addInput(
+                        label: 'Name',
+                        keyboard: KeyboardTypes.text,
+                        onsaved: (String value) {
+                          _newUser.nameSurname = value;
+                        },
+                        validator: FormGenerator.nameValidatorGenerator('your'),
+                      ),
+                      FormGenerator.addInput(
+                        label: 'Age',
+                        keyboard: KeyboardTypes.number,
+                        onsaved: (String value) {
+                          _newUser.age = int.parse(value);
+                        },
+                        validator: (String value) {
+                          if (value.isEmpty) {
+                            return 'Please enter your age! Exp: 21';
+                          }
+                        },
+                      ),
+                      FormGenerator.addDropdown(
+                          categories: ['Male', 'Female'],
+                          hint: 'Cinsiyet',
+                          onChanged: (value) {
+                            _newUser.gender = value;
                           },
-                          validator: FormGenerator.mailValidator,
-                        ),
-                        FormGenerator.addInput(
-                          label: 'Password',
-                          keyboard: KeyboardTypes.text,
-                          obsecure: true,
-                          onsaved: (String value) {
-                            _newUser.password = value;
-                          },
-                          validator: FormGenerator.passwordValidator,
-                          onchanged: (value) => _newUser.password = value,
-                        ),
-                        FormGenerator.addInput(
-                          label: 'Password',
-                          keyboard: KeyboardTypes.text,
-                          obsecure: true,
-                          validator: (String value) {
-                            if (value != _newUser.password) {
-                              return "Passwords does not match!";
+                          validator: (value) {
+                            if (value == null) {
+                              return 'Please select a gender!';
                             }
-                            return null;
-                          },
-                        ),
-                        FormGenerator.addInput(
-                          label: 'Name',
-                          keyboard: KeyboardTypes.text,
-                          onsaved: (String value) {
-                            _newUser.nameSurname = value;
-                          },
-                          validator: FormGenerator.nameValidatorGenerator('your'),
-                        ),
-                        FormGenerator.addInput(
-                          label: 'Age',
-                          keyboard: KeyboardTypes.number,
-                          onsaved: (String value) {
-                            _newUser.age = int.parse(value);
-                          },
-                          validator: (String value) {
-                            if (value.isEmpty) {
-                              return 'Please enter your age! Exp: 21';
-                            }
-                          },
-                        ),
-                        FormGenerator.addDropdown(
-                            categories: ['Male', 'Female'],
-                            hint: 'Cinsiyet',
-                            onChanged: (value) {
-                              _newUser.gender = value;
-                            },
-                            validator: (value) {
-                              if (value == null) {
-                                return 'Please select a gender!';
-                              }
-                            }),
-                        SizedBox(
-                          height: 8,
-                        ),
-                        Row(
+                          }),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: BaseCheckBox(
+                              value: _newUser.newsSetterConfirmation,
+                              onChanged: (value) {
+                                setState(() {
+                                  _newUser.newsSetterConfirmation = value;
+                                });
+                              },
+                            ),
+                          ),
+                          Expanded(
+                            child: Text(
+                              "Repet\’in bana özel kampanya, tanıtım ve fırsatlarından haberdar olmak istiyorum",
+                              overflow: TextOverflow.clip,
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 3,
+                      ),
+                      RichText(
+                        text: TextSpan(
+                          text: "Üye olmakla ",
+                          style: TextStyle(
+                            fontSize: 14.0,
+                            color: Color(0xFF6f6f6f),
+                            fontWeight: FontWeight.w600,
+                          ),
                           children: [
-                            Padding(
-                              padding: EdgeInsets.all(8.0),
-                              child: BaseCheckBox(
-                                value: _newUser.newsSetterConfirmation,
-                                onChanged: (value) {
-                                  setState(() {
-                                    _newUser.newsSetterConfirmation = value;
-                                  });
-                                },
+                            TextSpan(
+                              text: "Kullanım koşullarını ",
+                              style: TextStyle(
+                                color: kPrimaryColor,
                               ),
                             ),
-                            Expanded(
-                              child: Text(
-                                "Repet\’in bana özel kampanya, tanıtım ve fırsatlarından haberdar olmak istiyorum",
-                                overflow: TextOverflow.clip,
-                              ),
-                            )
+                            TextSpan(text: "onaylamış olursunuz.")
                           ],
                         ),
-                        SizedBox(
-                          height: 3,
-                        ),
-                        RichText(
-                          text: TextSpan(
-                            text: "Üye olmakla ",
-                            style: TextStyle(
-                              fontSize: 14.0,
-                              color: Color(0xFF6f6f6f),
-                              fontWeight: FontWeight.w600,
-                            ),
-                            children: [
-                              TextSpan(
-                                text: "Kullanım koşullarını ",
-                                style: TextStyle(
-                                  color: kPrimaryColor,
-                                ),
-                              ),
-                              TextSpan(text: "onaylamış olursunuz.")
-                            ],
+                      ),
+                      SizedBox(
+                        height: 3,
+                      ),
+                      RichText(
+                        text: TextSpan(
+                          text:
+                              "Kişisel verilerinize dair Aydınlatma Metni için  ",
+                          style: TextStyle(
+                            fontSize: 14.0,
+                            color: Color(0xFF6f6f6f),
+                            fontWeight: FontWeight.w600,
                           ),
-                        ),
-                        SizedBox(
-                          height: 3,
-                        ),
-                        RichText(
-                          text: TextSpan(
-                            text:
-                            "Kişisel verilerinize dair Aydınlatma Metni için  ",
-                            style: TextStyle(
-                              fontSize: 14.0,
-                              color: Color(0xFF6f6f6f),
-                              fontWeight: FontWeight.w600,
-                            ),
-                            children: [
-                              TextSpan(
-                                text: "tıklayınız.",
-                                style: TextStyle(
-                                  color: kPrimaryColor,
-                                ),
+                          children: [
+                            TextSpan(
+                              text: "tıklayınız.",
+                              style: TextStyle(
+                                color: kPrimaryColor,
                               ),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
                   ),
                 ),
-                SizedBox(
-                  height: height * 0.02,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    GestureDetector(
-                      child: Text(
-                        'Have you already an account?',
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodyText2
-                            .copyWith(color: kPrimaryColor),
-                      ),
-                      onTap: () {
-                        Navigator.pushReplacementNamed(
-                            context, LoginScreen.routeName);
-                      },
+              ),
+              SizedBox(
+                height: height * 0.02,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                    child: Text(
+                      'Have you already an account?',
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyText2
+                          .copyWith(color: kPrimaryColor),
                     ),
-                    BaseButton(
-                      text: 'Ileri',
-                      onPressed: () {
-                        if (_formKey.currentState.validate()) {
-                          _formKey.currentState.save();
-                          context.read<GeneralProviderData>().newUser = _newUser;
-                          Navigator.pushNamed(context, PetRegistrationScreen.routeName,);
-                        }
-                      },
-                      width: 120,
-                    ),
-                  ],
-                ),
-              ],
-            ),
+                    onTap: () {
+                      Navigator.pushReplacementNamed(
+                          context, LoginScreen.routeName);
+                    },
+                  ),
+                  BaseButton(
+                    text: 'Ileri',
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        _formKey.currentState.save();
+                        context.read<GeneralProviderData>().newUser = _newUser;
+                        Navigator.pushNamed(
+                          context,
+                          PetRegistrationScreen.routeName,
+                        );
+                      }
+                    },
+                    width: 120,
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
